95a589cd457b140c25b0a3a30ac6e13d0924f97b
Kevin Liu <kevin@potatofrom.space>
2017-11-04 11:11:13 -0400

    Add placeholder to upload to ipfs

--- src/Editor.js
+++ src/Editor.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import uuid from 'uuid/v4';
 
 class Editor extends Component {
-  click() {
-    window.location.href = "#" + uuid();
+  upload() {
+    // upload to ipfs
   }
   render() {
     return (
       <div>
         <h2>Editor</h2>
+        <textarea /><br />
+        <button onClick={this.upload}>Upload</button>
       </div>
     );
   }
