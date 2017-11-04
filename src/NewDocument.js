6d1588e6debc977cb249697b63ab728f097a87eb
Kevin Liu <kevin@potatofrom.space>
2017-11-04 15:14:55 -0400

    Fix IPFS saving

--- src/NewDocument.js
+++ src/NewDocument.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import IPFS from 'ipfs';
 
+
+
 class NewDocument extends Component {
+  constructor() {
+    super();
+    this.state = { status: "WAITING" };
+    this.handleFileUpload = this.handleFileUpload.bind(this);
+  }
   handleFileUpload(event) {
+    this.setState({ status: "UPLOADING" });
     const fr = new FileReader();
-    fr.onload = function() {
+    fr.onload = () => {
       const data = fr.result;
       const fileBuffer = new Buffer(data);
-      const node = new IPFS();
+      const node = new IPFS({
+        repo: String(Math.random() + Date.now())
+      });
       node.on('ready', () => {
         node.files.add({
           path: "/document",
           content: fileBuffer
-        }, function(err, files) {
+        }, (err, files) => {
+          this.setState({ status: "COMPLETE", hash: files[0].hash });
           console.log(err, files);
         });
       });
@@ -24,6 +35,10 @@ class NewDocument extends Component {
       <div>
         <h2>Upload Text File</h2>
         <input type="file" onChange={this.handleFileUpload} />
+        {this.state.status === "WAITING" && <p>Waiting for upload...</p>}
+        {this.state.status === "UPLOADING" && <p>Uploading...</p>}
+        {this.state.status === "COMPLETE" &&
+          <p>Document uploaded as <code>/ipfs/{this.state.hash}</code>.</p>}
       </div>
     );
   }
