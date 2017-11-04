dee7f5f52173a6927b8da822ffa9debb0109bad8
Kevin Liu <kevin@potatofrom.space>
2017-11-04 11:56:56 -0400

    Try to fix things

--- src/LitManager.js
+++ src/LitManager.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Web3 from 'web3';
 
-let web3;
-if (typeof web3 !== 'undefined') {
-    web3 = new Web3(web3.currentProvider);
-  } else {
-    // set the provider you want from Web3.providers
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-  }
+let localWeb3;
+if (typeof window.web3 !== 'undefined') {
+  localWeb3 = new Web3(window.web3.currentProvider);
+  console.log("Is MetaMask: " + (localWeb3.currentProvider.isMetaMask === true).toString());
+} else {
+  console.log("No injected Web3 detected.");
+  localWeb3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
 
 export default function LitManager() {
   return (
@@ -16,14 +17,14 @@ export default function LitManager() {
       <p>Initialized <code>Web3</code> object for easy interfacing of Ethereum JavaScript API.</p>
       <dl>
         <dt>Connected Ethereum node (Web3 provider)</dt>
-        <dd>{web3.currentProvider.host}</dd>
+        <dd>{localWeb3.currentProvider.host}</dd>
         <dt>Latest block</dt>
-        <dd>{web3.eth.blockNumber}</dd>
+        <dd>{localWeb3.eth.blockNumber}</dd>
         <dt>Accounts</dt>
         <dd>
           {Array.prototype.forEach(
             (account) => <div key={account}>{account}</div>
-          , web3.eth.accounts)}
+          , localWeb3.eth.accounts)}
         </dd>
       </dl>
     </div>
